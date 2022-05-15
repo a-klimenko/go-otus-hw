c9769ab32632a8577837124297eeacc6a50ff76d
Alexey Klimenko <alexey.klimenko55@gmail.com>
2022-05-15 21:07:06 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(data string) (string, error) {
+	var prev string
+	var result strings.Builder
+
+	if data == "" {
+		return "", nil
+	}
+
+	for i, val := range data {
+		count, err := strconv.Atoi(string(val))
+		if i == 0 && err == nil {
+			return "", ErrInvalidString
+		}
+
+		if i > 0 {
+			_, errPrev := strconv.Atoi(prev)
+			if err == nil && errPrev == nil {
+				return "", ErrInvalidString
+			}
+
+			if err == nil {
+				result.WriteString(strings.Repeat(prev, count))
+			} else if errPrev != nil {
+				result.WriteString(prev)
+			}
+		}
+		prev = string(val)
+	}
+	runes := []rune(data)
+	lastSym := string(runes[len(runes)-1:])
+
+	if _, err := strconv.Atoi(lastSym); err != nil {
+		result.WriteString(lastSym)
+	}
+
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "\n5", expected: "\n\n\n\n\n"},
+		{input: "_5bc", expected: "_____bc"},
+		{input: ".4b", expected: "....b"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,7 +38,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "a3b10"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
