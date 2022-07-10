153a50edf44115f6c6945b6de09b4ff7156df6a4
Alexey Klimenko <alexey.klimenko55@gmail.com>
2022-07-10 13:20:36 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,72 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var source *os.File
+	var dest *os.File
+	var err error
+
+	source, err = os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+	var fileInfo os.FileInfo
+	fileInfo, err = source.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dest, err = os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	if _, err := source.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	target := fileSize - offset
+	if target > limit && limit > 0 {
+		target = limit
+	}
+
+	bar := pb.StartNew(int(target))
+	defer bar.Finish()
+	bar.SetWriter(os.Stdout)
+	bar.Set(pb.Bytes, true)
+	bar.Set(pb.SIBytesPrefix, true)
+
+	buf := make([]byte, 1)
+	var progress int64
+	for progress < target {
+		read, readErr := source.Read(buf)
+		progress += int64(read)
+
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+
+		_, writeErr := dest.Write(buf)
+		if writeErr != nil {
+			return writeErr
+		}
+
+		if readErr == io.EOF {
+			break
+		}
+		if readErr != nil {
+			return readErr
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", "out.txt", 0, 0)
+		require.True(t, errors.Is(err, ErrUnsupportedFile))
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", 10000, 0)
+		require.True(t, errors.Is(err, ErrOffsetExceedsFileSize))
+	})
 }
