06282f68b2ff72b10cbcdac49ec1cd8b2e1a8670
Alexey Klimenko <alexey.klimenko55@gmail.com>
2022-07-17 14:58:49 +0300

    HW08 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	for varName, varParams := range env {
+		if varParams.NeedRemove {
+			os.Unsetenv(varName)
+		} else {
+			os.Setenv(varName, varParams.Value)
+		}
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			returnCode = exitError.ExitCode()
+		}
+	}
+
 	return
 }
