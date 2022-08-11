013a7f612b3dda4e7920509b781344737f508c2f
Alexey Klimenko <alexey.klimenko55@gmail.com>
2022-08-11 19:21:26 +0700

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,85 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeoutStr string
+
+func init() {
+	flag.StringVar(&timeoutStr, "timeout", "10s", "timeout to connect to the server (in seconds)")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var host, port string
+
+	switch {
+	case len(os.Args) < 3:
+		log.Fatalf("not enough arguments")
+	case len(os.Args) == 3:
+		host = os.Args[1]
+		port = os.Args[2]
+	default:
+		host = os.Args[2]
+		port = os.Args[3]
+	}
+
+	address := net.JoinHostPort(host, port)
+	timeout, err := time.ParseDuration(timeoutStr)
+	if err != nil {
+		log.Fatalf("timeout is invalid: %s", err)
+	}
+
+	client := NewTelnetClient(
+		address,
+		timeout,
+		os.Stdin,
+		os.Stdout,
+	)
+
+	if err := client.Connect(); err != nil {
+		log.Fatalf("Cannot connect: %v", err)
+	}
+	connectedMsg := []byte(fmt.Sprintf("...Connected to %s \n", address))
+	if _, err := os.Stderr.Write(connectedMsg); err != nil {
+		log.Fatal(err)
+	}
+
+	var closedMsg []byte
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Fatalf("Cannot close connection: %v", err)
+		}
+		if _, err := os.Stderr.Write(closedMsg); err != nil {
+			log.Fatalf("Cannot write to stderr: %v", err)
+		}
+	}()
+
+	res := make(chan error)
+	go func() {
+		res <- client.Receive()
+	}()
+
+	go func() {
+		res <- client.Send()
+	}()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGINT)
+	defer signal.Stop(sigint)
+
+	select {
+	case <-sigint:
+		closedMsg = []byte("...EOF \n")
+	case <-res:
+		closedMsg = []byte("...Connection was closed by peer \n")
+		close(sigint)
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
+)
+
+const ConnType = "tcp"
+
+var (
+	ErrInvalidIn  = errors.New("in is invalid")
+	ErrInvalidOut = errors.New("out is invalid")
 )
 
 type TelnetClient interface {
@@ -13,9 +23,60 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type Client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (c *Client) Connect() error {
+	var err error
+
+	if c.in == nil {
+		return ErrInvalidIn
+	}
+	if c.out == nil {
+		return ErrInvalidOut
+	}
+
+	c.conn, err = net.DialTimeout(ConnType, c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Close() error {
+	if err := c.conn.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) Send() error {
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return fmt.Errorf("failed to send message: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Client) Receive() error {
+	if _, err := io.Copy(c.out, c.conn); err != nil {
+		return fmt.Errorf("failed to receive message: %w", err)
+	}
+
+	return nil
+}
